7d524640ed1779ce241e63e3f2d7124de6689d96
Jin <46963276+JinMagician@users.noreply.github.com>
2023-07-25 16:36:37 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import re_path
 from loaderio.views import show_token
 
 
 urlpatterns = [
-    path(r'^(?P<token>loaderio-[0-9a-z]+)(?:\.txt|\.html|/)?$',
+    re_path(r'^(?P<token>loaderio-[0-9a-z]+)(?:\.txt|\.html|/)?$',
         show_token)
 ]
